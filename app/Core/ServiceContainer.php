96e6ba8dcbb7cd7dee54ce772cdd830eaf9ad5c6
tranthethang <tranthethang@gmail.com>
2020-04-24 08:04:57 +0700

    refactor

---
+++ app/Core/ServiceContainer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Origamiez\Core;
+
+class ServiceContainer
+{
+    /**
+     * @var \DI\Container
+     */
+    private $_container;
+
+    public function __construct()
+    {
+        $builder = new \DI\ContainerBuilder();
+        $builder->addDefinitions([]);
+        $builder->useAutowiring(true);
+        $builder->useAnnotations(false);
+        $this->_container = $builder->build();
+    }
+}
