73c7fc4096ce9f163845599415aa9683b64bc689
openwpdotorg <openwp.org@gmail.com>
2023-02-08 01:05:38 +0700

    attach vendor folder by default

--- functions.php
+++ functions.php
@@ -27,7 +27,13 @@ function origamiez_theme_setup()
 
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
-    add_theme_support('html5');
+    add_theme_support( 'html5', array(
+        'comment-list',
+        'comment-form',
+        'search-form',
+        'gallery',
+        'caption',
+    ));
     add_theme_support('loop-pagination');
     add_theme_support('automatic-feed-links');
     add_theme_support('post-formats', array('gallery', 'video', 'audio'));
