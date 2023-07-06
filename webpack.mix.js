44202ff87d8db80a36a73d34b706977620412bbe
thangtt-codluck <thangtt@codluck.com>
2023-07-06 11:41:51 +0700

    upgrade font-awesome 5

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,12 @@
 const mix = require('laravel-mix');
 
+mix.setPublicPath('origamiez/');
+
+// Main style.
 mix.sass('style.scss', 'origamiez/');
 
+// Bootstrap
 mix.sass('node_modules/bootstrap/scss/bootstrap.scss', 'origamiez/css/bootstrap.css')
+
+// Fontawesome
+mix.css('node_modules/@fortawesome/fontawesome-free/css/all.css', 'origamiez/css/fontawesome.css')
